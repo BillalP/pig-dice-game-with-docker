1fa8cc9391482ee86c0ad2091450c3561de79dd1
billalp <billal.patel@rightmove.co.uk>
2019-10-23 13:23:41 +0100

    Added hide css behaviour

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-/*
-  
-  GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's 
-  the next player's turn
-- The first player to reach 100 points as a GLOBAL score, wins the game
-
-*/
-
 var newGameBtn = document.querySelector('.btn-new');
 var rollDiceBtn = document.querySelector('.btn-roll');
 var holdBtn = document.querySelector('.btn-hold');
@@ -31,6 +18,8 @@ function newGame() {
 	newGameBtn.addEventListener('click', function() {
 		resetScores();
 		diceImage.style.display = 'block';
+		rollDiceBtn.style.display = 'block';
+		holdBtn.style.display = 'block';
 	});
 }
 
